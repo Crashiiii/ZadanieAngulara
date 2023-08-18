b4e01656fb5222f30280848e3a2660a5b0637c87
Crashiiii <97528066+Crashiiii@users.noreply.github.com>
2023-08-18 16:01:15 +0200

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,30 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContactComponent } from './pages/contact/contact.component';
+import { CalculatorComponent } from './pages/calculator/calculator.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
+import { MainComponent } from './pages/main/main.component';
+import { SamplesComponent } from './pages/samples/samples.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { VerifyEmailComponent } from './pages/verify-email/verify-email.component';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '/homepage',
-    pathMatch: 'full'
-  },
-  {
-    path: 'homepage',
-    component: HomepageComponent
-  },
-  {
-    path: 'contact',
-    component: ContactComponent
-  }
+  { path: 'homepage', component: HomepageComponent },
+  { path: 'calculator', component: CalculatorComponent },
+  { path: 'main', component: MainComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'samples', component: SamplesComponent },
+  { path: 'registration', component: RegistrationComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'verify-email', component: VerifyEmailComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: '', redirectTo: '/homepage', pathMatch: 'full' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './shared/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  isLoggedIn: boolean = false;
+
+  user$ = this.auth.user$;
+
+  constructor(private auth: AuthService) {
+    this.auth.user$.subscribe((user) => {
+      this.isLoggedIn = !!user;
+    });
+  }
+  ngOnInit(): void {}
+  
+  logout() {
+    this.auth.logout();
+  }
 }
